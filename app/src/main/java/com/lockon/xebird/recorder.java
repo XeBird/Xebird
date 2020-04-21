848fca53568a507f56fbdff9b1ab71a72f67872b
ljk5403 <ljk5403@outkook.com>
2020-04-21 21:29:23 +0800

    Initial commit

---
+++ app/src/main/java/com/lockon/xebird/recorder.java
@@ -0,0 +1,13 @@
+package com.lockon.xebird;
+
+public class recorder {
+    String recordFile="Bird_record";
+
+    public void addRecord(){}
+    public void rewriteRecord(){}
+    public void deleteRecord(){}
+    public void searchRecord(){}
+
+    public static void main (String args[]) {}
+
+}
