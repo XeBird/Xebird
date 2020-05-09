fb67d7731fd44ad8a7ec051509bcd9722cc6ec07
Guan810 <guan810@foxmail.com>
2020-05-09 22:50:10 +0800

    简单的按照名字搜索

--- app/src/main/java/com/lockon/xebird/FirstFragment.java
+++ app/src/main/java/com/lockon/xebird/FirstFragment.java
@@ -1,12 +1,13 @@
 package com.lockon.xebird;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,11 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+
+    private dbHelper dbH;
+    private TextView displaytext;
+    private EditText edittext;
+    private final String tag="FirstFragment";
 
     @Override
     public View onCreateView(
@@ -36,13 +42,30 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
-        TextView text= view.findViewById(R.id.textview_first);
-        String path=CopyDB.DB_PATH+"/"+CopyDB.DB_NAME;
-        SQLiteDatabase a=SQLiteDatabase.openOrCreateDatabase(path,null);
-        String sqlstatement="select * from MainDATA limit 5";
-        Cursor cursor=a.rawQuery(sqlstatement,null);
-        if(cursor!=null&&cursor.moveToFirst()){
-            text.setText(cursor.getString(cursor.getColumnIndex("NAME_CN")));
-        }
+        dbH=new dbHelper(getActivity());
+        edittext=view.findViewById(R.id.textview_edit);
+        displaytext=view.findViewById(R.id.textview_first);
+        displaytext.setText(getData("石鸡"));
+        view.findViewById(R.id.button_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name= String.valueOf(edittext.getText());
+                Log.d(tag, "onClick: "+Name+" was gotten!");
+                displaytext.setText(getData(Name));
+            }
+        });
     }
+
+    private String getData(String Name) {
+        Cursor cursor=dbH.findByName(Name,"MainDATA");
+        String visiText="";
+        if(cursor!=null&&cursor.moveToFirst()){
+            visiText=cursor.getString(cursor.getColumnIndex("NAME_CN"))
+            +"\t"+cursor.getString(cursor.getColumnIndex("NAME_EN"))
+            +"\n"+cursor.getString(cursor.getColumnIndex("NAME_LA"))
+            +"\n"+cursor.getString(cursor.getColumnIndex("MAIN_INFO"));
+        }
+        return visiText;
+    }
+
 }
